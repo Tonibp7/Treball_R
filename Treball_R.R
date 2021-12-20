70ea3b7f3a1936e6e283503606b8ed610ca4977d
heribertroig <91745480+heribertroig@users.noreply.github.com>
2021-12-20 17:39:19 +0100

    Update Treball_R.R

--- Treball_R.R
+++ Treball_R.R
@@ -38,6 +38,14 @@ print(paste0("In this dataset there are ", dim(Greenhouse_Gas_Emissions)[1], " o
 # d)Represent graphically co2, methane and nitrous oxide together by country and year. Can be possible all countries or a selection of a country, according to the request made.
 #Filter the top 25% most polluting countries
 not_countries <- c("Africa", "Asia", "Asia (excl. China & India)", "EU-27", "EU-28", "Europe", "Europe (excl. EU-27)", "Europe (excl. EU-28)", "International transport", "Kuwaiti Oil Fires", "Oceania", "North America", "North America (excl. USA)", "South America", "World" )
+
+# top_20_co2_countries <- Greenhouse_Gas_Emissions %>%
+#   select(country, co2, methane, nitrous_oxide, year) %>%
+#   filter(year >= 2010 & !(country %in% not_countries)) %>%
+#   arrange(desc(co2)) %>%
+#   group_by(country) %>%
+#   slice_head(n = 20)
+  
 Quart <- Greenhouse_Gas_Emissions %>%
   select(country, co2, methane, nitrous_oxide, year) %>%
   filter(year >= 2010 & !(country %in% not_countries)) %>%
@@ -45,7 +53,7 @@ Quart <- Greenhouse_Gas_Emissions %>%
 
 Greenhouse_Gas_Emissions %>%
   select(country, co2, methane, nitrous_oxide, year) %>%
-  filter(year >= 1970, co2 >= as.numeric(Quart[3,]) & !(country %in% not_countries)) %>%
+  filter(year >= 1970, co2 >= as.numeric(Quart[4,]) & !(country %in% not_countries)) %>%
    ggplot() + 
      geom_line(aes(x = year, y = co2, color = country)) #+
      #theme(legend.position = "none")
@@ -84,7 +92,7 @@ top_co2_countries <- last_decades %>%
   select(c(decade,country, year, co2)) %>%
   filter(!country %in% not_countries) %>%
   group_by(country) %>%
-  summarise(mean = mean(co2)) %>%
+  summarise(mean = mean(co2, na.rm = TRUE)) %>%
   arrange(desc(mean)) %>%
   top_n(10)
 
@@ -103,5 +111,44 @@ for (row in 1:nrow(top_co2_countries)) {
 }
 
 # do the same for the other metrics
-last_decade <- Greenhouse_Gas_Emissions %>%
-  filter(decade %in% c("1990-2000", "2000-2010", "2010-2020"))
+
+
+
+
+# EXERCISE 2
+
+
+
+
+
+
+# EXERCISE 3
+# 
+# a) Calculate the percentage increase between one decade and the following decade (eg. between 1990 - 
+#2000 and 2000-2010) for co2, methane and nitrous oxide together by each required country and
+# decade, offer a graph per country required and the average of all years studied. You should be able
+# to indicate what decades or countries it should represent.
+
+perc <- last_decades %>%
+  filter(!(country %in% not_countries)) %>%
+  group_by(decade) #%>%
+  #summarise()
+
+
+
+
+
+
+# b) Try to calculate the projection of co2, methane and nitrous oxide by country requiered. Use a
+# linear model to this computation (lm): For this, supposed that decade 1990-2000 has the value 1,
+# 2000-2010 the value of 2 and so consecutively so it can be transformed into a regression model.
+# You should be able to indicate what country it should represent. Use this example of prediction:
+#   https://stat.ethz.ch/R-manual/R-devel/library/stats/html/predict.lm.html
+
+
+
+
+
+
+# c) Comment on the results found and if you think they have effects on climate change. Optionally, you
+# can build a library to project the future of co2, methane and nitrous oxide and calculate statistics
